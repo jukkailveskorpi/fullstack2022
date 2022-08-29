fd03f6104190ce9a8e2e2a2c7aba978b081fb3a7
jukkailveskorpi <jukka.ilveskorpi@edu.sasky.fi>
2022-08-29 17:55:45 +0300

    1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -19,7 +19,7 @@ const App=()=> {
       <div>
       <Part part = {props.content} />
       <Part part = {props.content2} />
-      <Part part = {props.content2} />
+      <Part part = {props.content3} />
       </div>
     )
   }
@@ -31,18 +31,24 @@ const App=()=> {
   }
 
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React '
-  const exercises1 = 10
-  const part2 = 'Using props to pass data '
-  const exercises2 = 7
-  const part3 = 'State of a component '
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React ',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data ',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component ',
+    exercises: 14
+  }
 
   return (
   <div>
     <Header course={course} />
-    <Content content={part1 + exercises1} content2={part2 + exercises2} content3={part3 + exercises3} />
-    <Total total={exercises1 + exercises2 + exercises3} />
+    <Content content={part1.name + part1.exercises} content2={part2.name + part2.exercises} content3={part3.name + part3.exercises} />
+    <Total total={part1.exercises + part2.exercises + part3.exercises} />
   </div>
   )
 }
